96016139bdb44a04c0adf8f570d461d6baefcc1d
Alex Marks <alexpmarks7@gmail.com>
2020-11-29 09:57:27 +0000

    Updated main page image and added percentage bars to skills

--- CVJavascript.js
+++ CVJavascript.js
@@ -46,26 +46,91 @@ function hidePetroc()
   
 }
 
-function showBraunton()
-{
-    var lboro = document.getElementById("braunton")
-    if (lboro.style.display = 'none')
-    {
-        lboro.style.display = 'block';
+var i = 0;
+function moveCSharp() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("cSharpBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+   
+    function frame() {
+        if (width >= 80) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = (width/2 + "%");
+            elem.innerHTML = width + "%";
+        }
     }
-    var lboroShape = document.getElementById("brauntonShape")
-    lboroShape.style.height = 'min-height'
-  
+    
+  }
 }
 
-function hideBraunton()
-{
-    var lboro = document.getElementById("braunton")
-    if (lboro.style.display = 'block')
-    {
-        lboro.style.display = 'none';
+
+var i = 0;
+function moveHTML() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("HTMLBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+   
+    function frame() {
+        if (width >= 70) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = (width/2 + "%");
+            elem.innerHTML = width + "%";
+        }
     }
-    var lboroShape = document.getElementById("brauntonShape")
-    lboroShape.style.height = 'min-height'
-  
+    
+  }
+}
+
+var i = 0;
+function moveVS() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("VSBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+   
+    function frame() {
+        if (width >= 90) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = (width/2 + "%");
+            elem.innerHTML = width + "%";
+        }
+    }
+    
+  }
+}
+
+var i = 0;
+function moveGit() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("GitBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+   
+    function frame() {
+        if (width >= 80) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = (width/2 + "%");
+            elem.innerHTML = width + "%";
+        }
+    }
+    
+  }
 }
